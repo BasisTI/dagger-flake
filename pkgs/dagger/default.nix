97d63451bbbeb6cc24d24645a32e88fbd208acd7
Cedric Lamalle <cedric.lamalle@basis.com.br>
2025-09-05 09:15:05 -0300

    Upgrade dagger to v0.18.17

--- pkgs/dagger/default.nix
+++ pkgs/dagger/default.nix
@@ -6,23 +6,27 @@
 
 let
   pname = "dagger";
-  version = "0.18.16";
+  version = "0.18.17";
   platformInfo = {
     "x86_64-linux" = {
       arch = "linux_amd64";
-      sha256 = "sha256-bD4AsrmbEfKRhte6gXjLvJMGtGuytuoF9uFpj2Ol7cQ=";
+      sha256 = "sha256-pN+V2V2yNsLk5Jj+GAQIaNmAChg0mKwA8u9kqB2pPDY=";
     };
     "aarch64-linux" = {
       arch = "linux_arm64";
-      sha256 = "sha256-dobyQS7161IOobgB+DNsKMFbq6D9CIEZ/7lFv5g5JXE=";
+      sha256 = "sha256-UvDI7gF+BppKyjZ0cDa5Rz+M1L56ghqjOZ1IuMI9b+o=";
+    };
+    "armv7l-linux" = {
+      arch = "linux_armv7";
+      sha256 = "sha256-F1iCLkM3J4REkMkB8s6QxM7Fh63cEMv6v8eBTR8w6cQ=";
     };
     "x86_64-darwin" = {
       arch = "darwin_amd64";
-      sha256 = "sha256-H+rKkSaNvKgQtWhZksUi6hPMRfZlIae8yB+SWpx+LCM=";
+      sha256 = "sha256-nHop+bYdEw4bI4FvgYqDIi1R4q51kMGR94Akn1A+3+U=";
     };
     "aarch64-darwin" = {
       arch = "darwin_arm64";
-      sha256 = "sha256-zzShyzgVGBq5IgUm2RJwp2WZHRYC51BF5hn/zGbjEl8=";
+      sha256 = "sha256-k60U2NHj2+XN6+K57R/lG+l/YYZmcnJ9OmITnEpmdz0=";
     };
   };
   platform = lib.getAttr stdenv.hostPlatform.system platformInfo;
